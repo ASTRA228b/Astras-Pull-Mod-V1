514b4ded4076eed416cdd59287d31065e14621ee
M1 Interactive <36132911+m1llipede@users.noreply.github.com>
2026-02-21 07:25:16 -0600

    Updated Version in  Constantss.cs

--- New Mod/Stuff/Constants.cs
+++ New Mod/Stuff/Constants.cs
@@ -4,7 +4,7 @@ internal class Constantss
 {
     public const string GUID = "Astras.pull.mod";
     public const string Name = "AstrasPullModV1";
-    public const string Version = "0.0.1";
+    public const string Version = "0.1.1";
     public const string ObjectName = "Astras.pull";
 
 }
