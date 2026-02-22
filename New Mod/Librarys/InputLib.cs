b520e66852927f65415500acc9ca16b8ca4b5aef
M1 Interactive <36132911+m1llipede@users.noreply.github.com>
2026-02-22 11:27:20 -0600

    fixed joystick fr now

--- New Mod/Librarys/InputLib.cs
+++ New Mod/Librarys/InputLib.cs
@@ -25,7 +25,7 @@ internal class InputLib
             if (SteamVR_Actions.gorillaTag_RightJoystickClick == null)
                 return false;
 
-            return SteamVR_Actions.gorillaTag_RightJoystickClick.stateDown;
+            return SteamVR_Actions.gorillaTag_RightJoystickClick.state;
         }
     }
 
@@ -36,7 +36,7 @@ internal class InputLib
             if (SteamVR_Actions.gorillaTag_LeftJoystickClick == null)
                 return false;
 
-            return SteamVR_Actions.gorillaTag_LeftJoystickClick.stateDown;
+            return SteamVR_Actions.gorillaTag_LeftJoystickClick.state;
         }
     }
 }
